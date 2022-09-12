7a6835393aeb8f0d05fbf86dcaede262637b96ff
Theresa-o <okorotheresa19@gmail.com>
2022-09-12 14:54:22 +0100

    Updated necessary files for deployment to Railway

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -137,8 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
